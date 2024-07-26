d661a6d1d741ecf610881ad903c12075b0832d97
ElectricDosh <84617732+ElectricDosh@users.noreply.github.com>
2024-07-26 15:45:37 +0100

    Update threshsocket.go

--- internal/run/threshsocket.go
+++ internal/run/threshsocket.go
@@ -6,21 +6,22 @@ import (
 	"github.com/hectorgimenez/d2go/pkg/data/npc"
 	"github.com/hectorgimenez/koolo/internal/action"
 	"github.com/hectorgimenez/koolo/internal/config"
+	"github.com/hectorgimenez/koolo/internal/game"
 )
 
 type Threshsocket struct {
 	baseRun
 }
 
-func (t Threshsocket) Name() string {
+func (a Threshsocket) Name() string {
 	return string(config.ThreshsocketRun)
 }
 
-func (t Threshsocket) BuildActions() []action.Action {
-	actions := []action.Action{
-		t.builder.WayPoint(area.CrystallinePassage), // Moving to starting point (Crystalline Passage)
-		t.builder.MoveToArea(area.ArreatPlateau),
-		t.char.KillMonsterSequence(func(d game.Data) (data.UnitID, bool) {
+func (a Threshsocket) BuildActions() (actions []action.Action) {
+	return []action.Action{
+		a.builder.WayPoint(area.CrystallinePassage), // Moving to starting point (Crystalline Passage)
+		a.builder.MoveToArea(area.ArreatPlateau),
+		a.char.KillMonsterSequence(func(d game.Data) (data.UnitID, bool) {
 			if m, found := d.Monsters.FindOne(npc.BloodBringer, data.MonsterTypeSuperUnique); found {
 				return m.UnitID, true
 			}
@@ -28,9 +29,9 @@ func (t Threshsocket) BuildActions() []action.Action {
 
 			return 0, false
 		}, nil),
-		t.builder.ItemPickup(false, 35),
+		a.builder.ItemPickup(false, 35),
 		
 	}
 
-	return
+
 }
